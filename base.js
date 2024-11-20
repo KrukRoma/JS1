604698627c55ed610b95d86f727d02d1c85135ba
KrukX_rn50@student.itstep.org <KrukX_rn50@student.itstep.org>
2024-11-20 11:21:01 +0200

    first commit

---
+++ base.js
@@ -0,0 +1,99 @@
+//1
+// let number = prompt("Enter number: ");
+// number = Number(number);
+// let result = number ** 2;
+// alert(`The square of your number: ${result}`);
+
+//2
+// let number1 = prompt("Enter the first number:");
+// let number2 = prompt("Enter the second number:");
+// number1 = N5
+// umber(number1);
+// number2 = Number(number2);
+// let average = (number1 + number2) / 2;
+// alert(`The average of your numbers is: ${average}`);
+
+//3
+// let sideLength = prompt("Enter the side length of the square:");
+// sideLength = Number(sideLength);
+// let area = sideLength ** 2;
+// alert(`The area of the square is: ${area}`);
+
+//4
+// const KILOMETERS_TO_MILES = 0.621371;
+// let kilometers = prompt("Enter the distance in kilometers:");
+// kilometers = Number(kilometers);
+// let miles = kilometers * KILOMETERS_TO_MILES;
+// alert(`The distance in miles is: ${miles}`);
+
+//5
+// let number1 = prompt("Enter the first number:");
+// let number2 = prompt("Enter the second number:");
+
+// number1 = Number(number1);
+// number2 = Number(number2);
+
+// let sum = number1 + number2;
+// let difference = number1 - number2;
+// let product = number1 * number2;
+// let quotient = number1 / number2;
+
+// alert(`Sum: ${sum}\nDifference: ${difference}\nProduct: ${product}\nQuotient: ${quotient}`);
+
+//6
+// let a = prompt("Enter the value of a:");
+// let b = prompt("Enter the value of b:");
+
+// a = Number(a);
+// b = Number(b);
+
+// let x = -b / a;
+
+// alert(`The value of x is: ${x}`);
+
+//7
+// let currentHour = prompt("Enter the current hour (0-23):");
+// let currentMinute = prompt("Enter the current minute (0-59):");
+
+// currentHour = Number(currentHour);
+// currentMinute = Number(currentMinute);
+
+// let hoursLeft = 23 - currentHour;
+// let minutesLeft = 60 - currentMinute;
+
+// if (minutesLeft === 60) {
+//     hoursLeft++;
+//     minutesLeft = 0;
+// }
+
+// alert(`Time remaining until next day: ${hoursLeft} hours and ${minutesLeft} minutes.`);
+
+//8
+// let number = prompt("Enter a three-digit number:");
+
+// number = Number(number);
+
+// let secondDigit = Math.floor((number % 100) / 10);
+
+// alert(`The second digit of the number is: ${secondDigit}`);
+
+//9
+// let number = prompt("Enter a five-digit number:");
+
+// number = Number(number);
+
+// let lastDigit = number % 10;
+// let remainingDigits = Math.floor(number / 10);
+
+// let result = lastDigit * 10000 + remainingDigits;
+
+// alert(`The new number is: ${result}`);
+
+//10
+// let totalSales = prompt("Enter the total sales amount for the month:");
+
+// totalSales = Number(totalSales);
+
+// let salary = 250 + (0.10 * totalSales);
+
+// alert(`The total salary is: $${salary}`);
